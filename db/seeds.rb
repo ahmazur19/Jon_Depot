fc541ff3db21636117a3bc520161dc839406d8ff
Adam Mazur <adammazue@gmail.com>
2021-02-24 14:39:41 -0700

    Gems, seed.file and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+10.times do 
+	Department.create(
+		name: Faker::Construction.subcontract_category,
+
+	)
+
+end
+
+puts "seeded #{Department.all.size} departments"
